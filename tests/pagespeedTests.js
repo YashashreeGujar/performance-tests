db6c5c1e27350839a848ce00c95f2fe2e09e36e9
YashashreeGujar <yashashree.gujar@bbc.co.uk>
2023-11-19 14:07:36 +0000

    Removed console logging

--- tests/pagespeedTests.js
+++ tests/pagespeedTests.js
@@ -4,99 +4,78 @@ import testURLs from './testData.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
-describe('Performance Metrics Tests with PageSpeed Insights', function() {
+describe('Performance Metrics Tests with PageSpeed Insights', function () {
 
     this.timeout(100000); // Increase the default timeout for API call
 
     const apiKey = process.env.PAGE_SPEED_API_KEY;
 
+    let performanceData;
+
+    before(async () => {
+        try {
+            const response = await axios.get(`https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${testURLs[0]}&key=${apiKey}`);
+            performanceData = response.data.lighthouseResult.audits;
+        } catch (error) {
+            console.error(`Error fetching performance data:`, error);
+            // Handle the error, e.g., set performanceData to a default value or fail the suite.
+        }
+    });
 
     testURLs.forEach(testURL => {
-        describe(`Testing URL: ${testURL}`, function() {
-            let performanceData;
+        describe(`Testing URL: ${testURL}`, function () {
 
-            before(async () => {
-                const response = await axios.get(`https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${testURL}&key=${apiKey}`);
-                performanceData = response.data.lighthouseResult.audits;
-            });
-
-            it('Should have a good First Contentful Paint (FCP) metric', async() => {
+            // First Contentful Paint (FCP) metric
+            it('Should have a good First Contentful Paint (FCP) metric', async () => {
                 const fcpValue = performanceData['first-contentful-paint'].numericValue;
-                
-                
-                 // Displaying the FCP value and the expected threshold
-                 console.log(`Measured First Contentful Paint (FCP): ${fcpValue} milliseconds`);
-                 console.log("Expected FCP for a good user experience: 1800 milliseconds or less.");
-             
-                 // Check if the First Contentful Paint metric meets your threshold
-                 if (fcpValue <= 1800) {
-                     console.log("Test Passed: The FCP is within the acceptable range.");
-                 } else {
-                     console.log("Test Failed: The FCP is higher than the recommended value for a good user experience.");
-                 }
-             
-                 expect(fcpValue).to.be.lessThan(1800); // Expecting it to be less than 1.8 seconds
+                const expectedThreshold = 1800;
+
+                // Check if the FCP metric meets the threshold
+                expect(fcpValue, `FCP metric for ${testURL} is expected to be less than ${expectedThreshold} milliseconds.`).to.be.lessThan(expectedThreshold);
             });
-        
-            it('Should have a good Largest Contentful Paint (LCP) metric', async() => {
+
+            // Largest Contentful Paint (LCP) metric
+            it('Should have a good Largest Contentful Paint (LCP) metric', async () => {
                 const lcpValue = performanceData['largest-contentful-paint'].numericValue;
-                
-                
-                // Displaying the LCP value and the expected threshold
-                console.log(`Measured Largest Contentful Paint (LCP): ${lcpValue} milliseconds`);
-                console.log("Expected LCP for a good user experience: 2500 milliseconds or less.");
-            
-                // Check if the LCP metric meets your threshold
-                if (lcpValue <= 2500) {
-                    console.log("Test Passed: The LCP is within the acceptable range.");
-                } else {
-                    console.log("Test Failed: The LCP is higher than the recommended value for a good user experience.");
-                    console.error('Detailed Issues with LCP:', result.lhr.audits['largest-contentful-paint'].details);
-                }
-            
-                expect(lcpValue).to.be.lessThan(2500); // Expecting it to be less than 2.5 seconds
+                const expectedThreshold = 2500;
+
+                // Check if the LCP metric meets the threshold
+                expect(lcpValue, `LCP metric for ${testURL} is expected to be less than ${expectedThreshold} milliseconds.`).to.be.lessThan(expectedThreshold);
             });
-        
-           /* it('Should have a good Total Blocking Time (TBT) metric as a proxy for FID', async() => {
-                const tbtValue = performanceData['total-blocking-time'].numericValue;
-                
-                // Rest of your TBT logic goes here (similar to original)
-                // Displaying the TBT value and the expected threshold
-                console.log(`Measured Total Blocking Time (TBT): ${tbtValue} milliseconds`);
-                console.log("Expected TBT (as a proxy for FID) for a good user experience: 100 milliseconds or less.");
-            
-                // Check if the TBT metric meets your threshold
-                if (tbtValue <= 100) {
-                    console.log("Test Passed: The TBT is within the acceptable range.");
-                } else {
-                    console.log("Test Failed: The TBT is higher than the recommended value for a good user experience.");
-                    console.error('Detailed Issues with TBT:', result.lhr.audits['total-blocking-time'].details);
-                }
-            
-                expect(tbtValue).to.be.lessThan(100); // Expecting it to be less than 100ms
-            });*/
-            
-        
-            it('Should have a good Cumulative Layout Shift (CLS) metric', async() => {
+
+            // Cumulative Layout Shift (CLS) metric
+            it('Should have a good Cumulative Layout Shift (CLS) metric', async () => {
                 const clsValue = performanceData['cumulative-layout-shift'].numericValue;
-                
-                
-        
-                // Displaying the CLS value and the expected threshold
-                console.log(`Measured Cumulative Layout Shift (CLS): ${clsValue}`);
-                console.log("Expected CLS for a good user experience: 0.1 or less.");
-            
-                // Check if the CLS metric meets your threshold
-                if (clsValue <= 0.1) {
-                    console.log("Test Passed: The CLS is within the acceptable range.");
-                } else {
-                    console.log("Test Failed: The CLS is higher than the recommended value for a good user experience.");
-                    console.error('Detailed Issues with CLS:', result.lhr.audits['cumulative-layout-shift'].details);
-                }
-            
-                expect(clsValue).to.be.lessThanOrEqual(0.1); // Expecting it to be 0.1 or less
+                const expectedThreshold = 0.1;
+
+                // Check if the CLS metric meets the threshold
+                expect(clsValue, `CLS metric for ${testURL} is expected to be less than or equal to ${expectedThreshold}`).to.be.lessThanOrEqual(expectedThreshold);
             });
-        
+
+            it('Should have a good Total Blocking Time (TBT) metric as a proxy for FID', async () => {
+                const tbtValue = performanceData['total-blocking-time'].numericValue;
+                const expectedThreshold = 200;
+
+                // Check if the TBT metric meets the threshold
+                expect(tbtValue, `TBT metric for ${testURL} is expected to be less than ${expectedThreshold} ms`).to.be.lessThan(expectedThreshold);
+            });
+
+            it('Should have a good First Input Delay (FID) metric', async () => {
+                const fidValue = performanceData['max-potential-fid'].numericValue;
+                const expectedThreshold = 100;
+
+                // Check if the FID metric meets the threshold
+                expect(fidValue, `FID metric: ${fidValue} ms, expected to be less than ${expectedThreshold} ms`).to.be.lessThan(expectedThreshold);
+            });
+
+            it('Should have a good Interaction to Next Paint (INP) metric', async () => {
+                const inpValue = performanceData['interactive'].numericValue;
+                const expectedThreshold = 200;
+
+                // Check if the INP metric meets the threshold
+                expect(inpValue, `INP metric for ${testURL} is expected to be less than or equal to ${expectedThreshold} ms`).to.be.lessThanOrEqual(expectedThreshold);
+            });
+
         });
     });
 });
--- tests/testData.js
+++ tests/testData.js
@@ -1,20 +1,25 @@
 
 const testURLs = [
     'https://www.bbc.co.uk/bitesize/subjects',
-    'https://www.bbc.co.uk/bitesize/subjects/z3kw2hv',
+    'https://www.bbc.co.uk/bitesize/subjects',
+    'https://www.bbc.co.uk/bitesize/subjects/zv48q6f',
     'https://www.test.bbc.co.uk/bitesize/preview/subjects/z6hs34j',
     'https://www.bbc.co.uk/bitesize/subjects/zxpy4wx',
     'https://www.bbc.co.uk/bitesize/subjects/zfw9bdm',
     'https://www.bbc.co.uk/bitesize/levels/zbr9wmn',
     'https://www.bbc.co.uk/bitesize/levels/zbr9wmn/year/zncsscw',
     'https://www.bbc.co.uk/bitesize/study-support',
+    'https://www.bbc.co.uk/bitesize/groups/cd5exmm663et',
+    'https://www.bbc.co.uk/bitesize/groups/cd5exmm663et',
+    'https://www.bbc.co.uk/bitesize/groups/c306433371zt',
     'https://www.bbc.co.uk/bitesize/levels/z4kw2hv',
     'https://www.bbc.co.uk/bitesize/levels/z3g4d2p',
     'https://www.bbc.co.uk/bitesize/levels/z98jmp3',
+    'https://www.bbc.co.uk/bitesize/post-16',
+    'https://www.bbc.co.uk/bitesize/secondary',
     'https://www.bbc.co.uk/bitesize/levels/zfxmtv4',
     'https://www.bbc.co.uk/bitesize/secondary-games',
-    'https://www.bbc.co.uk/bitesize/collections/primary-games/1',
-    'https://www.bbc.co.uk/bitesize/post-16'
+    'https://www.bbc.co.uk/bitesize/collections/primary-games/1'
 ];
 
 export default testURLs;
